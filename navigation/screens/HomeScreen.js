d04679012cf0188163b8c33aae635d51729e4ead
Eric Tao <erictao04@gmail.com>
2024-01-21 08:17:15 -0800

    semi-working

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import Square from "./Square";
 import { useState, useEffect } from "react";
-import { getImages } from "../../utils/getImages"
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../../config'
-
+import { getImages, getImage } from "../../utils/getImages";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../config";
 
 const Home = () => {
   const [user] = useAuthState(auth);
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState("");
 
   useEffect(() => {
-    setImages(getImages(user.email));
-    console.log(images)
+    getImages(user.email, setImages);
+    // getImage(setImages);
   }, []);
 
   return (
@@ -23,57 +22,47 @@ const Home = () => {
         <RenderSquare />
       </ScrollView>
     </View>
-  )
+  );
 
-  function RenderSquare(){
-    var imageloop = []
-    for (let i=0; i < images.length; i++) {
+  function RenderSquare() {
+    var imageloop = [];
+    for (let i = 0; i < 6; i++) {
       imageloop.push(
-        <View style={styles.item}>
-          <Square image={images[i]}/>
+        <View key={i} style={styles.item}>
+          <Square image={images[i]} />
         </View>
-      )
+      );
     }
-    return (
-      <View style={styles.boxContainer}>
-        {imageloop}
-      </View>
-    )
-  }
-}
+    console.log("render");
 
+    return <View style={styles.boxContainer}>{imageloop}</View>;
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'col',
-    flexWrap: 'wrap',
-    alignItems: 'stretch', // if you want to fill rows left to right
-    backgroundColor: 'white',
+    flexDirection: "col",
+    flexWrap: "wrap",
+    alignItems: "stretch", // if you want to fill rows left to right
+    backgroundColor: "white",
   },
-  boxContainer:{
+  boxContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
   },
-  item: {    
-  },
+  item: {},
 
   title: {
     fontSize: 30,
-    fontWeight: 'bold',
-    backgroundColor: 'white',
-    textAlign: 'left',
+    fontWeight: "bold",
+    backgroundColor: "white",
+    textAlign: "left",
     paddingLeft: 14,
     paddingBottom: 10,
     paddingVertical: 10,
-    
-    
   },
-   
 });
 export default Home;
-
-
-
--- utils/getImages.js
+++ utils/getImages.js
@@ -2,7 +2,40 @@ import { ref, get, child } from "firebase/database";
 import { ref as storageRef, getBlob } from "firebase/storage";
 import { auth, db, storage } from "../config";
 
-export function getImages(email) {
+const blobToBase64 = (blob, setImage) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
+export async function getImage(setImages) {
+  blob = await getBlob(
+    storageRef(storage, "erictao04@gmail.com-1705832192755")
+  );
+  base64Url = await blobToBase64(blob);
+  setImages(base64Url);
+}
+
+export async function getImages(email, setImages) {
+  setImages([
+    "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+    "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+    "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg",
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  ]);
+  // setImages([
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  //   "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII",
+  // ]);
+  return;
   // get(child(ref(db), `sellerClothes`))
   //   .then((snapshot) => {
   //     if (snapshot.exists()) {
@@ -23,18 +56,17 @@ export function getImages(email) {
     "erictao04@gmail.com-1705832153877",
     "erictao04@gmail.com-1705832136381",
   ];
-  images = {};
+  images = [];
   //   getBlob(storageRef(storage, "erictao04@gmail.com-1705832192755"))
   //     .then((blob) => blob.arrayBuffer())
   //     .then((buffer) => console.log(buffer));
-
+  filereader = new FileReader();
   for (let imageId of imageIds) {
-    getBlob(storageRef(storage, imageId)).then((blob) => {
-      console.log("downloaded");
-      images[imageId] = blob;
-      console.log(images);
-    });
+    blob = await getBlob(storageRef(storage, imageId));
+    base64Url = await blobToBase64(blob);
+    images.push(base64Url);
+    console.log(imageId);
   }
-
-  return images;
+  //   console.log(images);
+  setImages(images);
 }

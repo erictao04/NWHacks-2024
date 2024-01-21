82084e63877bfef9509d4c9e71a84033fed006e4
Rachel Wang <rachelwang0432@gmail.com>
2024-01-20 19:05:27 -0800

    rebase

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,14 +1,64 @@
-import * as React from 'react';
-import { View, Text} from 'react-native';
+// import * as React from 'react';
+// import { View, Text, Image, StylSheet} from 'react-native';
 
-export default function HomeScreen({navigation}) {
-    return(
-        <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold'}}>Home
-            </Text>
-        </View>
-    )
 
-}
+// const home =() => {
+//     return (
+//         <>
+//         <View>
+//             <Text style={{fontSize: 20, fontWeight: 700, color: 'green'}}>Products</Text>
+//         </View>  
+      
+//         {/* <View style={styles.container}>
+//           <Image
+//           style = {styles.image}
+//           source={require('./blueShirt.JPG')}/>
+//          </View> */}
+
+//          <View>
+//           <Text>Hi</Text>
+//          </View>
+//         </>
+//     )
+// }
+
+
+
+
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+const home = () => (
+  <View style={styles.container}>
+   
+    <Text style={styles.title}>Clothes 1:</Text>
+    
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+
+
+
+
+
+export default home;

00f56e6c87683f59fa013ea406e70d2919214108
Rachel Wang <rachelwang0432@gmail.com>
2024-01-21 00:44:41 -0800

    make home screen

---
+++ navigation/screens/DetailsScreen.js
@@ -0,0 +1,13 @@
+// Create a new file: DetailsScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DetailsScreen = () => (
+    <View>
+    <Text>HELLO!</Text>
+    {/* Add your additional content here */}
+  </View>
+  
+);
+
+export default DetailsScreen;
